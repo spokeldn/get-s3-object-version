b42fcade97d800b42962b22c03fc9d22af46e981
Gareth Bishop <gareth@spoke-london.com>
2022-09-02 15:56:13 +0100

    prune unused args

--- dist/index.js
+++ dist/index.js
@@ -27118,21 +27118,13 @@ const S3 = __nccwpck_require__(1258);
 const s3 = new S3();
 
 async function run() {
-  console.log('beginning action');
-
   try {
-    console.log('setup client');
-
     const params = {
       Bucket: core.getInput('bucket'),
       Key: core.getInput('object_key')
     };
 
-    console.log(JSON.stringify(params));
-
     const object = await s3.headObject(params).promise();
-
-    console.log(object);
 
     core.setOutput('version_id', object.VersionId);
   } catch (error) {
--- index.js
+++ index.js
@@ -4,21 +4,13 @@ const S3 = require('aws-sdk/clients/s3');
 const s3 = new S3();
 
 async function run() {
-  console.log('beginning action');
-
   try {
-    console.log('setup client');
-
     const params = {
       Bucket: core.getInput('bucket'),
       Key: core.getInput('object_key')
     };
 
-    console.log(JSON.stringify(params));
-
     const object = await s3.headObject(params).promise();
-
-    console.log(object);
 
     core.setOutput('version_id', object.VersionId);
   } catch (error) {
