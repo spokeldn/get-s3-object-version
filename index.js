ec6001c982df49c5c6a2cb4f22f7848c74bac7b7
Gareth Bishop <gareth@spoke-london.com>
2022-09-02 12:01:10 +0100

    add more debugging

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 const { S3Client, HeadObjectCommand } = require('@aws-sdk/client-s3');
 
 async function run() {
+  console.log('beginning action');
   try {
     const client = new S3Client({
       region: core.getInput('region'),
@@ -12,16 +13,23 @@ async function run() {
       }
     });
 
+    console.log('setup client');
+
     const params = {
       Bucket: core.getInput('bucket'),
       Key: core.getInput('object_key')
     };
 
+    console.log(JSON.stringify(params));
+
     const command = new HeadObjectCommand(params);
     const response = await client.send(command);
 
+    console.log(response);
+
     core.setOutput('version_id', response.VersionId);
   } catch (error) {
+    console.error(error)
     core.setFailed(error.message);
   }
 }
