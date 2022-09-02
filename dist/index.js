c51d8e883c60414743044ebff0a34568ff403dc2
Gareth Bishop <gareth@spoke-london.com>
2022-09-02 15:36:30 +0100

    update dist

--- dist/index.js
+++ dist/index.js
@@ -27123,6 +27123,8 @@ const s3 = new S3({
 
 async function run() {
   console.log('beginning action');
+  console.log(core.getInput('aws_access_key'));
+  console.log(core.getInput('aws_secret_key'));
   try {
     console.log('setup client');
 
