83a871007cc6072fc7a0f942fb5f86a8b5dac016
Gareth Bishop <gareth@spoke-london.com>
2022-09-02 14:23:32 +0100

    update dist

--- dist/index.js
+++ dist/index.js
@@ -27113,11 +27113,10 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const core = __nccwpck_require__(2085);
-// const github = require('@actions/github');
-// const { S3Client, HeadObjectCommand } = require('@aws-sdk/client-s3');
 const S3 = __nccwpck_require__(1258);
 
 const s3 = new S3({
+  region: core.getInput('region'),
   accessKeyId: core.getInput('aws_access_key'),
   secretAccessKey: core.getInput('aws_secret_key')
 });
@@ -27125,14 +27124,6 @@ const s3 = new S3({
 async function run() {
   console.log('beginning action');
   try {
-    // const client = new S3Client({
-    //   region: core.getInput('region'),
-    //   credentials: {
-        // accessKeyId: core.getInput('aws_access_key'),
-        // secretAccessKey: core.getInput('aws_secret_key')
-    //   }
-    // });
-
     console.log('setup client');
 
     const params = {
@@ -27143,9 +27134,6 @@ async function run() {
     console.log(JSON.stringify(params));
 
     const object = await s3.headObject(params).promise();
-
-    // const command = new HeadObjectCommand(params);
-    // const response = await client.send(command);
 
     console.log(object);
 
