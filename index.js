75749b82eee321fdf53df4dae3f8c91b0b57ff75
Gareth Bishop <gareth@spoke-london.com>
2022-09-02 14:22:13 +0100

    clean up commented out code, specify region to s3 config

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
-// const { S3Client, HeadObjectCommand } = require('@aws-sdk/client-s3');
 const S3 = require('aws-sdk/clients/s3');
 
 const s3 = new S3({
+  region: core.getInput('region'),
   accessKeyId: core.getInput('aws_access_key'),
   secretAccessKey: core.getInput('aws_secret_key')
 });
@@ -11,14 +10,6 @@ const s3 = new S3({
 async function run() {
   console.log('beginning action');
   try {
-    // const client = new S3Client({
-    //   region: core.getInput('region'),
-    //   credentials: {
-        // accessKeyId: core.getInput('aws_access_key'),
-        // secretAccessKey: core.getInput('aws_secret_key')
-    //   }
-    // });
-
     console.log('setup client');
 
     const params = {
@@ -29,9 +20,6 @@ async function run() {
     console.log(JSON.stringify(params));
 
     const object = await s3.headObject(params).promise();
-
-    // const command = new HeadObjectCommand(params);
-    // const response = await client.send(command);
 
     console.log(object);
 
