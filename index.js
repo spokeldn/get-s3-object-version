3a6e6e7ff70f161bfd26189ad2a61cb49a962549
Gareth Bishop <gareth@spoke-london.com>
2022-09-02 10:26:48 +0100

    update action code to run in a normal async function

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { S3Client, HeadObjectCommand } = require('@aws-sdk/client-s3');
 
-const client = new S3Client({});
+async function run() {
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
 
-(async () => {
+  const client = new S3Client({});
+
   try {
     const params = {
       Bucket: core.getInput('bucket'),
@@ -15,10 +18,9 @@ const client = new S3Client({});
     const response = await client.send(command);
 
     core.setOutput('version_id', response.VersionId);
-
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
   } catch (error) {
     core.setFailed(error.message);
   }
-})();
+}
+
+run();
